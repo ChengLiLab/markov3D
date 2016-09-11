f9adfd03df6531946c3b72e79fdb0621d4e45717
wangyn <wangyn@knight.nis.cbi>
2016-09-12 00:14:17 +0800

    inference

--- markov3d/R/estimate_transition_mat.R
+++ markov3d/R/estimate_transition_mat.R
@@ -26,3 +26,47 @@ EstimateTransitionMat <- function(ice.matrix, distance.matrix,
   }
   return(transition.mat)
 }
+
+
+DistanceCounts <- function(hic_mat, resolution) {
+  #' Transform Hi-C full matrix.
+  #' @param hic_mat matrix, hic full matrix
+  #' @param resolution int, the matrix's resolution
+  #' @return a data.table with two columns. The first column is the linear 
+  #' genomic distance between two bins and the second is the number of counts
+  #' mapped.
+  counts_dist <- NULL
+  mat_dim <- ncol(hic_mat)
+  print('Transforming...')
+  for (i in 1:(mat_dim - 1)) {
+    if (i %% 100 == 0) {
+      sprintf('----------%f%%', i / mat_dim * 100)
+    }
+    genomic_distance <- resolution * (i - 1)
+    sub_mat <- hic_mat[1:(mat_dim + 1 - i), i:mat_dim]
+    counts <- diag(sub_mat)
+    tmp_dt <- data.table(
+      distance = genomic_distance,
+      norm_counts = counts
+    )
+    counts_dist <- rbind(counts_dist, tmp_dt)
+  }
+  return(counts_dist)
+}
+
+
+PowerLawFitting <- function(dt) {
+  #' Fit a power law function between the linear genomic distance and counts
+  #' @param dt data.table, the first column is genomic distance and the second
+  #' is counts
+  #' @return the coefficient of the power law function
+  
+  # remove 0
+  dt <- dt + 1
+  
+  # fit the model and return the coefficient
+  power_model <- lm(log(dt[[1]]) ~ log(dt[[2]]))
+  return(power_model$coefficients[2])
+}
+
+
